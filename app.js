f4ac1037828b825cb7bb3e77f72cefb4495098dd
James Rosa <JROSA10.tv@gmail.com>
2023-06-24 19:14:48 -0400

    resetbutton

--- app.js
+++ app.js
@@ -8,17 +8,19 @@ btns.forEach(function (btn) {
   const styles = e.currentTarget.classList;
   if (styles.contains("decrease")) {
    count--
-  }
-  value.textContent = count;
- })
-})
-
-btns.forEach(function (btn) {
- btn.addEventListener('click', function (e) {
-  const styles = e.currentTarget.classList;
-  if (styles.contains("increase")) {
+  } else if (styles.contains("increase")) {
    count++;
+  } else {
+   count = 0;
   }
-  value.textContent = count
+  if (count > 0) {
+   value.style.color = "green"
+  }
+  if (count < 0) {
+   value.style.color = "red"
+  }
+  if (count === 0)
+   value.style.color = "black"
+  value.textContent = count;
  })
 })
