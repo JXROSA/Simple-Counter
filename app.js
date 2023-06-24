db9fa84bd82f6d7d1e83d328ec92951e5969c31c
James Rosa <JROSA10.tv@gmail.com>
2023-06-24 19:05:33 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+// set initial value 
+let count = 0;
+const value = document.querySelector('#value');
+const btns = document.querySelectorAll('.btn');
+
+btns.forEach(function (btn) {
+ btn.addEventListener('click', function (e) {
+  const styles = e.currentTarget.classList;
+  if (styles.contains("decrease")) {
+   count--
+  }
+  value.textContent = count;
+ })
+})
+
+btns.forEach(function (btn) {
+ btn.addEventListener('click', function (e) {
+  const styles = e.currentTarget.classList;
+  if (styles.contains("increase")) {
+   count++;
+  }
+  value.textContent = count
+ })
+})
